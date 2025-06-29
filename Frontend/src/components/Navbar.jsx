35d59940b926d61ec8cf96df69cccd8764f8e0f7
Nitin Yadav <ydvnitin16@gmail.com>
2025-06-29 15:56:25 +0530

    Fix: case sensitive-import

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import logo from '../assets/logo.png';
+import logo from '../assets/Logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faSearch,
