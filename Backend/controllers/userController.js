237089e528d749cbbf461bb65a46a32fd713b5f2
Nitin Yadav <ydvnitin16@gmail.com>
2025-06-29 21:45:36 +0530

    Some deployment debugging

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -65,8 +65,8 @@ const loginUser = async (req, res) => {
 
         res.cookie('authHeader', `Bearer ${token}`, {
             httpOnly: true, // sent only to http
-            secure: process.env.NODE_ENV === 'production', // cookie only set to the https
-            sameSite: 'lax', // accessed by same domain
+            secure: true, // cookie only set to the https
+            sameSite: 'none', // accessed by same domain
             maxAge: 3 * 24 * 60 * 60 * 1000, // Expires in 3 Days
         });
 
